3189c80796c17d287ea103b1e5699626089ce7c2
AYUSH YADAV <raosahab.ay1203@gmail.com>
2025-10-08 18:33:36 +0000

    datatypes and variables

---
+++ basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// alert(3+3) we're using nodejs, not a browser
+
+console.log(3 + 3); 
+
+console.log(typeof null); 
---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const accountid = 123456
+let accountname = "Ayush"
+var accountpassword = "101010"
+accountCity="GKP" 
+let accountstate ;
+
+//accountid=2 not allowed
+accountname="Ankit" 
+accountpassword="121212"
+accountCity="DELHI"
+
+console.table([accountid,accountname,accountpassword,accountCity])  
+console.log(accountstate);
+
