8a335967b5e544637bec391423c5fd5ac691dc9d
AYUSH YADAV <raosahab.ay1203@gmail.com>
2025-11-04 17:34:28 +0000

    commit

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -1,12 +1,12 @@
 //let scores="33abc"
-//let score= undefined
+//let score= undefined or null
 //console.log(typeof score)
 //console.log(typeof (score))
 
 //let valueInNumber=Number(score)
 //console.log(typeof valueInNumber)
 //console.log(valueInNumber)
-
-let isLoggedIn = 1
+//let isLoggedIns = 1
+let isLoggedIn = 0
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
