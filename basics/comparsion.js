466b7d8f26bd8d5cfea79dce2aca4dc6257bce52
AYUSH YADAV <raosahab.ay1203@gmail.com>
2025-11-06 16:14:27 +0000

    commit

--- basics/comparsion.js
+++ basics/comparsion.js
@@ -1,6 +1,8 @@
-// console.log(4>'3');
-// console.log(3<4); 
+// // // console.log(4>'3');
+// // // console.log(3<4); 
 
-console.log(null>0);
-console.log(null>=0);
-console.log(null==0);
+// // console.log(null>0);
+// // console.log(null>=0);
+// // console.log(null==0);
+
+// console.log('2'===2);
