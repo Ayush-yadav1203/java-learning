685e4c628daf2bf1fe5ff09a2f3486c9099b8c1b
AYUSH YADAV <raosahab.ay1203@gmail.com>
2025-11-12 13:37:47 +0000

    num math

---
+++ basics/num_and_math.js
@@ -0,0 +1,30 @@
+// const score=400
+// const balance = new Number(400)
+
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(score)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+// const otherNumber=100000000
+// console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toLocaleString())
+// console.log(otherNumber.toLocaleString("en-IN"))
+
+
+// math +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-55))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.random())
+// console.log((Math.random()*100)+1)
+
+const min=10
+const max=20
+
+console.log(Math.random()*(max-min+1)) 
+console.log(Math.floor(Math.random()*(max-min+1))+min)
